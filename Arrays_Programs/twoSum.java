7a645302cd192ee72fd639b6455b07c4a1c6fb23
Anuj1143 <aanujmaurya8127@gmail.com>
2025-04-17 12:20:55 +0530

    new file add

---
+++ Arrays_Programs/twoSum.java
@@ -0,0 +1,30 @@
+ package Arrays_Programs;
+
+import java.util.Scanner;
+
+class twoSum {
+    public static int  pairSum(int arr[], int target){
+        int sum=0;
+        for(int i=0;i<arr.length;i++){
+            for(int j=i+1;j<arr.length;j++){
+                if(arr[i]+arr[j]==target){
+                    sum+=1;
+                }
+            }
+        }
+        return sum;
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+         for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+         }
+         int target=sc.nextInt();
+         int ans=pairSum(arr, target);
+         System.out.println(ans);
+    }
+    
+} 
+
